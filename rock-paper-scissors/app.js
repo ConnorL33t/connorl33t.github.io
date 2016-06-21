9bf6c20af0142ad43069980356d23fd7da45bcc9
Connor Lee <Connorlee@u.boisestate.edu>
2016-06-21 13:27:56 -0600

    right version

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,12 +1,18 @@
-function play(x){
-  var choices = ['rock', 'paper', 'scissors','lizard','spock']
+function play(xstring){
+  var choices = ['rock', 'scissors','lizard','paper','spock']
   var randI = Math.floor(Math.random() * choices.length)
   var compChoice = choices[randI]
+  var x = Number(xstring);
   document.getElementById('rpsls').innerHTML = compChoice;
-  if(x!=compChoice){
-
-    
-}else{
-  alert('you tied');
-}   
+  if(choices[x]!=compChoice){
+if((compChoice=== choices[(x+1)%5]) || (compChoice === choices[(x+2)%5])){
+  alert('You won!');
+}else
+{
+  alert('you lost');
+}
+}
+else{
+ alert('you tied');
+}
 }
